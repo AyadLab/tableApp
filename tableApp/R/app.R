195c42e6a3a25491a061746843b83edc3f97c8cd
rithvikveera <98138584+rithvikveera@users.noreply.github.com>
2022-07-14 18:08:49 -0400

    Add files via upload

---
+++ tableApp/R/app.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(tableApp)
+
+tableApp <- function() {
+  ui <- fluidPage(
+    actionButton("squareButton", "Square"),
+    # imageOutput("squareImage")
+    conditionalPanel(condition = "input.squareButton >= 1",
+                     fluidRow(
+                       column(12,
+                              dataTableOutput('table')
+                       )
+                     )
+    )
+  )
+  
+  server <- function(input, output) {
+    output$table <- renderDataTable(iris)
+  }
+  shinyApp(ui, server)
+}
